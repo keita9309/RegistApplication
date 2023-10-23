9d8647fec7ae30744278036a1b5cf9b42fab742a
Keita <herausforderer09@gmail.com>
2023-10-24 01:28:57 +0900

    画面遷移時のform詰め替え処理の追加(使用経験のあるPG言語)

--- src/main/java/sample/a002/controller/A002Controller.java
+++ src/main/java/sample/a002/controller/A002Controller.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import sample.a001.form.A001Form;
+import sample.a002.constants.A002Constants;
 import sample.a002.form.A002Form;
+import sample.common.cd.CD001;
 
 @Controller
 public class A002Controller {
@@ -62,46 +64,55 @@ public class A002Controller {
 	 * @return a001Form 詰め替え後のForm
 	 */
 	private A001Form createNew001Form(A001Form a001Form) {
+		final CD001 workExFlg_1 = CD001.WorkExFlg_1;
+		final CD001 workExFlg_0 = CD001.WorkExFlg_0;
 		// 実務経験の有無
 		switch (a001Form.getWorkExperienceFlg()) {
-		case "1":
-			a001Form.setWorkExperienceFlg("あり");
+		case A002Constants.ONE:
+			a001Form.setWorkExperienceFlg(workExFlg_1.getWorkExperienceFlg());
 			break;
-		case "0":
-			a001Form.setWorkExperienceFlg("なし");
+		case A002Constants.ZERO:
+			a001Form.setWorkExperienceFlg(workExFlg_0.getWorkExperienceFlg());
 			break;
 		}
+		
 		// 現在の職種
 		switch (a001Form.getTypeOfOccupationKbn()) {
-		case "1":
+		case A002Constants.ONE:
 			a001Form.setTypeOfOccupationKbn("PM / PL");
 			break;
-		case "2":
+		case A002Constants.TWO:
 			a001Form.setTypeOfOccupationKbn("SE");
 			break;
-		case "3":
+		case A002Constants.THREE:
 			a001Form.setTypeOfOccupationKbn("PG");
 			break;
-		case "4":
+		case A002Constants.FOUR:
 			a001Form.setTypeOfOccupationKbn("その他");
 			break;
 		}
-		// 使用した事のあるプログラミング言語
-//		switch (a001Form.getTypeOfOccupationKbn()) {
-//		case "1":
-//			a001Form.setTypeOfOccupationKbn("PM / PL");
-//			break;
-//		case "2":
-//			a001Form.setTypeOfOccupationKbn("SE");
-//			break;
-//		case "3":
-//			a001Form.setTypeOfOccupationKbn("PG");
-//			break;
-//		case "4":
-//			a001Form.setTypeOfOccupationKbn("その他");
-//			break;
-//		}
 		
+		// 使用した事のあるプログラミング言語
+		int count = 0;
+		for(String num : a001Form.getExperiencedProgrammingLungage()) {
+			
+			switch (num) {
+			case A002Constants.ONE:
+				a001Form.getExperiencedProgrammingLungage().set(count, A002Constants.PRO_LUNG_JAVA);
+				break;
+			case A002Constants.TWO:
+				a001Form.getExperiencedProgrammingLungage().set(count, A002Constants.PRO_LUNG_PHP);
+				break;
+			case A002Constants.THREE:
+				a001Form.getExperiencedProgrammingLungage().set(count, A002Constants.PRO_LUNG_RUBY);
+				break;
+			case A002Constants.FOUR:
+				a001Form.getExperiencedProgrammingLungage().set(count, A002Constants.PRO_LUNG_ELSE);
+				break;
+			}
+			count++;
+		}
+
 		return a001Form;
 	}
 }
