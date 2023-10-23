d31dac8e645da7709ab6df63d46ebc0b3d365f29
Keita <herausforderer09@gmail.com>
2023-10-24 01:26:33 +0900

    定数ファイルの作成

---
+++ src/main/java/sample/a002/constants/A002Constants.java
@@ -0,0 +1,23 @@
+package sample.a002.constants;
+
+public class A002Constants {
+	
+	public static final String ZERO = "0";
+	
+	public static final String ONE = "1";
+	
+	public static final String TWO = "2";
+
+	public static final String THREE = "3";
+	
+	public static final String FOUR = "4";
+	
+	public static final String PRO_LUNG_JAVA = "Java";
+	
+	public static final String PRO_LUNG_PHP = "PHP";
+	
+	public static final String PRO_LUNG_RUBY = "Ruby";
+	
+	public static final String PRO_LUNG_ELSE = "その他";
+
+}
--- src/main/java/sample/common/cd/CD001.java
+++ src/main/java/sample/common/cd/CD001.java
@@ -1,6 +1,26 @@
 package sample.common.cd;
 
 public enum CD001 {
+	WorkExFlg_1("1", "あり"),
+	WorkExFlg_0("0", "なし"),
+	;
 	
+	private String code; // コード
+    private String workExperienceFlg; // 経験の有無
+    
+    /**
+     * コンストラクタ
+     * かならずprivate指定でないといけない
+     */
+     private CD001 (String code, String workExperienceFlg) {
+         this.code = code;
+         this.workExperienceFlg = workExperienceFlg;
+     }
+     public String getCode(){
+         return code;
+     }
+     public String getWorkExperienceFlg(){
+         return workExperienceFlg;
+     }
 
 }
